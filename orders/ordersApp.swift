6335822f76d3d082f87263cc4c4b55b00ac3ad33
Valentina <valentina@cleverplusplus.com>
2025-05-15 13:19:13 +0300

    Initial Commit

---
+++ orders/ordersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ordersApp.swift
+//  orders
+//
+//  Created by Valentina Ungurean on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ordersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ordersTests/ordersTests.swift
@@ -0,0 +1,17 @@
+//
+//  ordersTests.swift
+//  ordersTests
+//
+//  Created by Valentina Ungurean on 15.05.2025.
+//
+
+import Testing
+@testable import orders
+
+struct ordersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
